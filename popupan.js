23e2f90511a952a270b7a0edfaf1a1bf8dce59f8
trungnguyen7694 <68918254+trungnguyen7694@users.noreply.github.com>
2020-10-08 23:08:41 +0700

    Update popupan.js

--- popupan.js
+++ popupan.js
@@ -1,8 +1,8 @@
 
-	var url_popup = 'https://nhadatvetinh.com';
+	var url_popup = 'https://hlreal.com.vn';
 
       //<![CDATA[
-	  // var url_ppp = 'http://bit.ly/1KmPNYe';
+	  // var url_ppp = 'https://hlreal.com.vn/';
       function addEvent(obj, eventName, func){
         if (obj.attachEvent)
         {
